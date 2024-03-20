a231c6908bf20843baec87790c95e6cd6cc17864
subham swarup pradhan <subhamswaruppradhan@subhams-MacBook-Air.local>
2024-03-20 12:42:18 +0530

    ok6

--- app.js
+++ app.js
@@ -12,17 +12,19 @@ require("dotenv").config({ path: "config/config.env" });
 // Using Middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors())
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  next();
-});
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   next();
+// });
 
 app.use(cookieParser());
 
