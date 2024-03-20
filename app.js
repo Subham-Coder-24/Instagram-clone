7a4ec60065cd339dd61f7294813ab3cb872efd01
subham swarup pradhan <subhamswaruppradhan@subhams-MacBook-Air.local>
2024-03-20 12:47:03 +0530

    ok10

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ require("dotenv").config({ path: "config/config.env" });
 // Using Middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
 
 // app.use(
 //   cors({
@@ -26,7 +27,7 @@ app.use(cors())
 //   next();
 // });
 
-app.use(cookieParser());
+
 
 // Importing Routes
 const post = require("./routes/post");
